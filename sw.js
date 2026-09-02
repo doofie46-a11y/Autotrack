f6422b6db57f239586df3fdaec83b9cd2c1cffae
WedGallery <doofie46@gmail.com>
2026-09-02 15:40:24 +0000

    Fix PWA per hosting su dominio root (non piu' sotto-path GitHub Pages)
    - manifest.json: start_url/scope da /Autotrack/ a /
    - sw.js: percorsi asset da /Autotrack/... a /, bump cache v14->v15
    - index.html: aggiunta favicon (icon-192/512, apple-touch-icon)

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE = 'autotrack-v13';
+const CACHE = 'autotrack-v15';
 const ASSETS = [
-  '/Autotrack/',
-  '/Autotrack/index.html',
-  '/Autotrack/manifest.json',
-  '/Autotrack/icon-192.png',
-  '/Autotrack/icon-512.png',
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png',
   'https://fonts.googleapis.com/css2?family=Bebas+Neue&family=DM+Sans:wght@300;400;500;600&family=JetBrains+Mono:wght@400;600&display=swap'
 ];
 
